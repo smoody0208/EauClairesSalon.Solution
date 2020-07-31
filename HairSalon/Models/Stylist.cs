1c61f1719a20a32b3036447b09b39004e09e23c1
Spencer Moody <spencer.moody@outlook.com>
2020-07-31 09:16:37 -0700

    Add HashSet to Stylist model for Client info, and ICollection

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -5,8 +5,13 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
+    public Stylist() 
+    {
+      this.Client = new HashSet<Client>();
+    }
     public int StylistId { get; set; }
     public string Name { get; set; }
     public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
