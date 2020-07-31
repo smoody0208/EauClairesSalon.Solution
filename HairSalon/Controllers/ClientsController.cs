ce289dd7c1e0a9103ae4b908c7214fee84d41b42
Spencer Moody <spencer.moody@outlook.com>
2020-07-31 10:07:12 -0700

    Add ClientsController constructor to ClientsController

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
